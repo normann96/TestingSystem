c4671e8f0f29fb96f0c5dc9dfc560645d90dc7c0
Александр <alexfeodosia@yandex.ru>
2018-05-16 23:08:06 +0300

    Added NinjectModule for Dependency Injection

---
+++ TestingSystem.BLL/Infrastructure/IdentityNinjectModule.cs
@@ -0,0 +1,33 @@
+﻿using Ninject.Modules;
+using Ninject.Web.Common;
+using TestingSystem.BLL.Interfaces;
+using TestingSystem.BLL.Services;
+using TestingSystem.DAL.EF;
+using TestingSystem.DAL.Interfaces;
+using TestingSystem.DAL.Interfaces.IRepository;
+using TestingSystem.DAL.Repositories;
+
+namespace TestingSystem.BLL.Infrastructure
+{
+    public class IdentityNinjectModule : NinjectModule
+    {
+        private readonly string _connectionString;
+
+        public IdentityNinjectModule(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public override void Load()
+        {
+            Bind<AppIdentityDbContext>().To<AppIdentityDbContext>().InRequestScope().WithConstructorArgument(_connectionString);
+            Bind<IIdentityUnitOfWork>().To<IdentityUnitOfWork>().InRequestScope();
+
+            Bind<IUserService>().To<UserService>();
+            Bind<IRoleService>().To<RoleService>();
+
+            Bind<IUserRepository>().To<UserRepository>();
+            Bind<IRoleRepository>().To<RoleRepository>();
+        }
+    }
+}
