98c209339f269e00dc35036d27f03a9307c43788
Александр <alexfeodosia@yandex.ru>
2018-05-17 10:22:24 +0300

    Update bootstrap to v4.1.1

--- TestingSystem.DAL/Identity/AppUserManager.cs
+++ TestingSystem.DAL/Identity/AppUserManager.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using TestingSystem.DAL.EF;
 using TestingSystem.DAL.Entities;
 
 namespace TestingSystem.DAL.Identity
@@ -7,6 +10,35 @@ namespace TestingSystem.DAL.Identity
     {
         public AppUserManager(IUserStore<AppUser> store) : base(store)
         {
+        }
+
+        public static AppUserManager CreateWithConfig(AppIdentityDbContext db)
+        {
+            AppUserManager manager = new AppUserManager(new UserStore<AppUser>(db));
+
+            // Config user login validator
+            manager.UserValidator = new UserValidator<AppUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // Config user password validator
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 4,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = true,
+                RequireLowercase = false,
+                RequireUppercase = false,
+            };
+
+            // Config user Lockout
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+            return manager;
         }
     }
 }
