0349121f593b97e8cce5e7ead2d24f4a16cc2637
Александр <alexfeodosia@yandex.ru>
2018-05-17 22:32:25 +0300

    Update Account View Model

--- TestingSystem.WEB/Models/Account/LoginViewModel.cs
+++ TestingSystem.WEB/Models/Account/LoginViewModel.cs
@@ -4,11 +4,11 @@ namespace TestingSystem.WEB.Models.Account
 {
     public class LoginViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Required field Login")]
         [Display(Name = "Login")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Required field Password")]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
