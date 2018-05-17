36926bb6fd6f0be33403fdffad069ed3b1d3ea19
Александр <alexfeodosia@yandex.ru>
2018-05-17 22:30:31 +0300

    Added user view models

---
+++ TestingSystem.WEB/Models/Users/CreateUserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestingSystem.WEB.Models.Users
+{
+    public class CreateUserViewModel
+    {
+        [Required(ErrorMessage = "Required field Login")]
+        [Display(Name = "Login")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required field Email")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required field Password")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Required field Confirm Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ TestingSystem.WEB/Models/Users/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestingSystem.WEB.Models.Users
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        [Display(Name = "Name")]
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
