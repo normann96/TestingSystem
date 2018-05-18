40e99c12cfe2aca505c028cc2bcd8d8d981bcdc4
Александр <alexfeodosia@yandex.ru>
2018-05-18 17:25:25 +0300

    [WEB] Added Models for Roles

---
+++ TestingSystem.WEB/Models/Roles/RoleEditModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TestingSystem.BLL.EntitiesDto;
+
+namespace TestingSystem.WEB.Models.Roles
+{
+    public class RoleEditModel
+    {
+        public RoleDto Role { get; set; }
+        public IEnumerable<UserDto> Members { get; set; }
+        public IEnumerable<UserDto> NonMembers { get; set; }
+    }
+}
---
+++ TestingSystem.WEB/Models/Roles/RoleEditViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestingSystem.WEB.Models.Roles
+{
+    public class RoleEditViewModel
+    {
+        [Required(ErrorMessage = "Required field RoleName")]
+        public string RoleName { get; set; }
+
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
+}
