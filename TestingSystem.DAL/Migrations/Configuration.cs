1f5f184cc0464708addef78578848bcf6511f230
Александр <alexfeodosia@yandex.ru>
2018-05-18 18:40:45 +0300

    [DAL] Enable-Migrations, Add-Migration Initial

---
+++ TestingSystem.DAL/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace TestingSystem.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TestingSystem.DAL.EF.AppIdentityDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "TestingSystem.DAL.EF.AppIdentityDbContext";
+        }
+
+        protected override void Seed(TestingSystem.DAL.EF.AppIdentityDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
