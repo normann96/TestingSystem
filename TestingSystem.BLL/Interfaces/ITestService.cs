23e22a9b05fdf5aeecf9cab27e09ebbb6deaca8c
Александр <alexfeodosia@yandex.ru>
2018-05-18 19:53:45 +0300

    [BLL] Added ITestService and TestService

---
+++ TestingSystem.BLL/Interfaces/ITestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestingSystem.BLL.EntitiesDto;
+
+namespace TestingSystem.BLL.Interfaces
+{
+    public interface ITestService : IDisposable
+    {
+        Task<TestDto> GetByIdAsync(int? testId);
+        Task<List<TestDto>> GetAllAsync();
+        Task CreateAsync(TestDto testDto);
+        Task UpdateAsync(TestDto testDto);
+        Task DeleteAsync(int testId);
+    }
+}
---
+++ TestingSystem.BLL/Services/TestService.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestingSystem.BLL.EntitiesDto;
+using TestingSystem.BLL.Interfaces;
+using TestingSystem.DAL.Entities;
+using TestingSystem.DAL.Interfaces;
+
+namespace TestingSystem.BLL.Services
+{
+    public class TestService : ITestService
+    {
+        private ITestUnitOfWork Database { get; }
+
+        public TestService(ITestUnitOfWork uow)
+        {
+            this.Database = uow ?? throw new ArgumentNullException(nameof(uow));
+        }
+
+        #region ITestService members
+
+        public async Task<TestDto> GetByIdAsync(int? testId)
+        {
+            var test = await Database.TestRepository.GetByIdAsync(testId);
+            if (test == null)
+                return null;
+
+            var testDto = new TestDto
+            {
+                Id = test.Id,
+                Name = test.Name,
+                TestDescription = test.TestDescription,
+                Questions = new List<QuestionDto>()
+            };
+
+            foreach (var question in test.Questions)
+            {
+                var newQuest = new QuestionDto
+                {
+                    Id = question.Id,
+                    QuestionContent = question.QuestionContent,
+                    Point = question.Point,
+                    TestId = question.TestId,
+                    Answers = new List<AnswerDto>()
+                };
+                testDto.Questions.Add(newQuest);
+            }
+            return testDto;
+        }
+
+        public async Task<List<TestDto>> GetAllAsync()
+        {
+            var tests = await Database.TestRepository.GetAllAsync();
+            var testDtos = new List<TestDto>();
+            foreach (var test in tests)
+            {
+                testDtos.Add(new TestDto
+                {
+                    Id = test.Id,
+                    Name = test.Name,
+                    TestDescription = test.TestDescription,
+                });
+            }
+            return testDtos;
+        }
+
+        public async Task CreateAsync(TestDto testDto)
+        {
+            var test = new Test
+            {
+                Name = testDto.Name,
+                TestDescription = testDto.TestDescription,
+            };
+            var testResult = Database.TestRepository.Create(test);
+            await Database.SaveAsync();
+        }
+
+        public async Task UpdateAsync(TestDto testDto)
+        {
+            var test = await Database.TestRepository.GetByIdAsync(testDto.Id);
+            if (test == null) return;
+
+            test.Name = testDto.Name;
+            test.TestDescription = testDto.TestDescription;
+
+            Database.TestRepository.Update(test);
+            await Database.SaveAsync();
+        }
+
+        public async Task DeleteAsync(int testId)
+        {
+            var test = await Database.TestRepository.GetByIdAsync(testId);
+            if (test == null)
+                return;
+
+            var testResult = Database.TestRepository.Delete(test);
+            await Database.SaveAsync();
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            Database?.Dispose();
+        }
+    }
+}
