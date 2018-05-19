bc8d772b437d0ba23d39a7138a4dd1e1eedf9bc7
Александр <alexfeodosia@yandex.ru>
2018-05-20 00:09:08 +0300

    [DAL] Added repository for test result

---
+++ TestingSystem.DAL/Interfaces/IRepository/ITestResultRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TestingSystem.DAL.Entities;
+
+namespace TestingSystem.DAL.Interfaces.IRepository
+{
+    public interface ITestResultRepository : IRepository<TestResult>
+    {
+        Task<TestResult> GeTestResultAsync(string userId, int testId);
+    }
+}
---
+++ TestingSystem.DAL/Repositories/TestResultRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using TestingSystem.DAL.EF;
+using TestingSystem.DAL.Entities;
+using TestingSystem.DAL.Interfaces.IRepository;
+using TestingSystem.DAL.Repositories.Base;
+
+namespace TestingSystem.DAL.Repositories
+{
+    public class TestResultRepository : BaseRepository<TestResult>, ITestResultRepository
+    {
+        public TestResultRepository(AppIdentityDbContext context) : base(context)
+        {
+        }
+
+        public async Task<TestResult> GeTestResultAsync(string userId, int testId)
+        {
+            var testResult = await Set.Include(x => x.QuestionResults).FirstOrDefaultAsync(x => x.TestId == testId && x.UserId == userId);
+            return testResult;
+        }
+    }
+}
