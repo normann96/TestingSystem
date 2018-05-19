5d851875da369e8dca2960d4c4a825ccf99f8ad8
Александр <alexfeodosia@yandex.ru>
2018-05-20 00:09:33 +0300

    [DAL] Added repository for question result

---
+++ TestingSystem.DAL/Interfaces/IRepository/IQuestionResultRepository.cs
@@ -0,0 +1,9 @@
+﻿using TestingSystem.DAL.Entities;
+
+namespace TestingSystem.DAL.Interfaces.IRepository
+{
+    public interface IQuestionResultRepository : IRepository<QuestionResult>
+    {
+        
+    }
+}
---
+++ TestingSystem.DAL/Repositories/QuestionResultRepository.cs
@@ -0,0 +1,14 @@
+﻿using TestingSystem.DAL.EF;
+using TestingSystem.DAL.Entities;
+using TestingSystem.DAL.Interfaces.IRepository;
+using TestingSystem.DAL.Repositories.Base;
+
+namespace TestingSystem.DAL.Repositories
+{
+    public class QuestionResultRepository : BaseRepository<QuestionResult>, IQuestionResultRepository
+    {
+        public QuestionResultRepository(AppIdentityDbContext context) : base(context)
+        {
+        }
+    }
+}
