fbddef0789a9db7c7870a75c17f06014c2fc5da3
Александр <alexfeodosia@yandex.ru>
2018-05-20 00:17:55 +0300

    [WEB] Update test controller

--- TestingSystem.WEB/Controllers/TestController.cs
+++ TestingSystem.WEB/Controllers/TestController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using TestingSystem.BLL.EntitiesDto;
 using TestingSystem.BLL.Interfaces;
+using TestingSystem.BLL.Services;
 using TestingSystem.WEB.Models.Tests;
 
 namespace TestingSystem.WEB.Controllers
@@ -14,10 +17,14 @@ namespace TestingSystem.WEB.Controllers
     public class TestController : Controller
     {
         private ITestService TestService { get; }
+        private IQuestionService QuestionService { get; }
+        private IResultService ResultService { get; }
 
-        public TestController(ITestService testService)
+        public TestController(ITestService testService, IQuestionService questionService, IResultService resultService)
         {
             TestService = testService ?? throw new ArgumentNullException(nameof(testService));
+            QuestionService = questionService ?? throw new ArgumentNullException(nameof(questionService));
+            ResultService = resultService ?? throw new ArgumentNullException(nameof(resultService));
         }
 
         // GET: Test
@@ -25,11 +32,11 @@ namespace TestingSystem.WEB.Controllers
         {
             var tests = await TestService.GetAllAsync();
             var testsView = tests.Select(test => new TestViewModel
-                {
-                    Id = test.Id,
-                    Name = test.Name,
-                    TestDescription = test.TestDescription,
-                }).ToList();
+            {
+                Id = test.Id,
+                Name = test.Name,
+                TestDescription = test.TestDescription,
+            }).ToList();
 
             return View(testsView);
         }
@@ -109,7 +116,7 @@ namespace TestingSystem.WEB.Controllers
             var testDto = await TestService.GetByIdAsync(id.Value);
             if (testDto == null)
                 return HttpNotFound();
-            
+
 
             TestViewModel testViewModel = new TestViewModel
             {
@@ -153,6 +160,123 @@ namespace TestingSystem.WEB.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> RunTest(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var testDto = await TestService.GetByIdAsync(id.Value);
+            if (testDto == null)
+                return HttpNotFound();
+
+            var questionDto = await QuestionService.GetFirstByTestIdAsync(id.Value);
+
+            if (questionDto == null)
+                return HttpNotFound("Question not found");
+
+            var questionView = new QuestionViewModel
+            {
+                Id = questionDto.Id,
+                QuestionContent = questionDto.QuestionContent,
+                Point = questionDto.Point,
+                TestId = questionDto.TestId,
+                Answers = new List<AnswerViewModel>()
+            };
+            foreach (var answerDto in questionDto.Answers)
+            {
+                var answerView = new AnswerViewModel
+                {
+                    Id = answerDto.Id,
+                    QuestionId = answerDto.QuestionId,
+                    AnswerContent = answerDto.AnswerContent,
+                    IsTrue = answerDto.IsTrue
+                };
+                questionView.Answers.Add(answerView);
+            }
+
+
+            if (User.Identity.IsAuthenticated)
+            {
+                string userId = User.Identity.GetUserId();
+                await ResultService.CreateTestResult(userId, testDto.Id);
+            }
+
+            ViewBag.TestName = testDto.Name;
+            return View("ShowQuestion", questionView);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> GoNextQuestion([Bind(Exclude = "Answers")] QuestionViewModel model, string testName, int[] answersId)
+        {
+            if (ModelState.IsValid)
+            {
+                if (User.Identity.IsAuthenticated)
+                {
+                    string userId = User.Identity.GetUserId();
+                    await ResultService.SaveQuestionResult(userId, model.TestId, model.Id, answersId);
+                }
+
+                var questionDto = await QuestionService.GetNextQuestion(model.TestId, model.Id);
+                if (questionDto == null)
+                    return RedirectToAction("GetResult", new { testId = model.TestId });
+
+                var questionView = new QuestionViewModel
+                {
+                    Id = questionDto.Id,
+                    QuestionContent = questionDto.QuestionContent,
+                    Point = questionDto.Point,
+                    TestId = questionDto.TestId,
+                    Answers = new List<AnswerViewModel>()
+                };
+                foreach (var answerDto in questionDto.Answers)
+                {
+                    var answerView = new AnswerViewModel
+                    {
+                        Id = answerDto.Id,
+                        QuestionId = answerDto.QuestionId,
+                        AnswerContent = answerDto.AnswerContent,
+                        IsTrue = answerDto.IsTrue
+                    };
+                    (questionView.Answers as List<AnswerViewModel>)?.Add(answerView);
+                }
+
+                var testDto = await TestService.GetByIdAsync(model.TestId);
+
+                ViewBag.TestName = testDto.Name;
+                return View("ShowQuestion", questionView);
+            }
+            ViewBag.TestName = testName;
+            return View("ShowQuestion", model);
+        }
+
+        public async Task<ActionResult> GetResult(int testId)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string userName = User.Identity.GetUserName();
+                var test = await TestService.GetByIdAsync(testId);
+
+                if (User.Identity.IsAuthenticated)
+                {
+                    string userId = User.Identity.GetUserId();
+                    var resultDto = await ResultService.GetTestResultAsync(userId, testId);
+                    var resultTest = new ResultViewModel
+                    {
+                        TestName = test.Name,
+                        Score = resultDto.SummaryResult,
+                        UserName = userName,
+                        IsPassed = resultDto.IsPassed
+                    };
+                    return View(resultTest);
+                }
+
+            }
+            return View("Error");
+        }
 
         protected override void Dispose(bool disposing)
         {
