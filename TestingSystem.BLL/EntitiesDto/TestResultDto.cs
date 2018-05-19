c26156648b690fe7ca305a02e116bc5d782206d8
Александр <alexfeodosia@yandex.ru>
2018-05-20 00:14:52 +0300

    [BLL] Added entity for test result

---
+++ TestingSystem.BLL/EntitiesDto/TestResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TestingSystem.BLL.EntitiesDto
+{
+    public class TestResultDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int TestId { get; set; }
+        public float SummaryResult { get; set; }
+        public bool IsPassed { get; set; }
+    }
+}
