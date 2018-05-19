c1c326c4901b402b49e67655554efa45768a42f0
Александр <alexfeodosia@yandex.ru>
2018-05-20 00:06:45 +0300

    [DAL] Added entities for results

---
+++ TestingSystem.DAL/Entities/QuestionResult.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestingSystem.DAL.Entities
+{
+    public class QuestionResult
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int TestResultId { get; set; }
+        public TestResult TestResult { get; set; }
+
+        [Required]
+        public bool IsTrueAnswer { get; set; }
+        public int QuestionId { get; set; }
+        public virtual Question Question { get; set; }
+    }
+}
---
+++ TestingSystem.DAL/Entities/TestResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestingSystem.DAL.Entities
+{
+    public class TestResult
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public int TestId { get; set; }
+
+        public float SummaryResult { get; set; }
+
+        public ICollection<QuestionResult> QuestionResults { get; set; }
+    }
+}
